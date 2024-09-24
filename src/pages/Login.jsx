de436f456c501ecfc3a8e1060450d377013d4f0a
indialifah <indialifah25@gmail.com>
2024-09-24 10:54:19 +0700

    add logic to login and register account

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
 
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleLogin = () => {
+    const payload = {
+      email: email,
+      password: password,
+    }
+    console.log(payload)
+  }
 
 
   return (
     <div className='flex flex-row min-h-screen justify-center items-center'>
-        <div className='bg-white w-2/5 p-16 shadow-xl rounded-xl'>
+        <div className='bg-white bg-opacity-80 w-2/5 p-16 shadow-xl rounded-xl'>
             <h1 className='text-5xl text-black font-medium text-center mb-16'>Artfolio</h1>
             <div className='flex flex-col gap-4 mb-10'>
                 
-                <input type="text" placeholder='Username or E-mail'
+                <input type="email" placeholder='E-mail' onChange={handleChangeEmail}
                 className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
 
-                <input type="password" placeholder='Password'
+                <input type="password" placeholder='Password' onChange={handleChangePassword}
                 className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
 
-                <button type="submit"
+                <button type="submit" onClick={handleLogin}
                 className='py-3 bg-orange text-white hover:bg-peach hover:text-black font-medium rounded-md'>
                     Login
                 </button>
                 <div className="flex gap-1 justify-center md:text-sm text-xs">
                     <p className='text-black'>Don't have an account?</p>
-                    <a to='/register' className="underline text-orange hover:text-peach cursor-pointer">Create new</a>
+                    <Link to='/register' className="underline text-orange hover:text-peach cursor-pointer">Create new</Link>
                 </div>
 
             </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Register = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleRegister = () => {
+    const payload = {
+      username: username,
+      password: password,
+    }
+    console.log(payload)
+  }
+
   return (
     <div className='flex flex-row min-h-screen justify-center items-center'>
         <div className='bg-white bg-opacity-80 w-2/5 p-16 shadow-xl rounded-xl'>
             <h1 className='text-5xl text-black font-medium text-center mb-16'>Artfolio</h1>
             <div className='flex flex-col gap-4 mb-10'>
                 
-                <input type="text" placeholder='Username or E-mail' 
+                <input type="email" placeholder='E-mail' onChange={handleChangeEmail}
                 className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
 
-                <input type="password" placeholder='Password' 
+                <input type="password" placeholder='Password' onChange={handleChangePassword}
                 className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
 
-                <button type="submit" 
+                <button type="submit" onClick={handleRegister}
                 className='py-3 bg-orange text-white hover:bg-peach hover:text-black font-medium rounded-md'>
                     Register
                 </button>
