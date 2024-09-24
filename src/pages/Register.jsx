4e74cb4e07c9818b34918fb1010272bcb91547c7
indialifah <indialifah25@gmail.com>
2024-09-24 11:50:11 +0700

    edit Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -25,24 +25,45 @@ const Register = () => {
   return (
     <div className='flex flex-row min-h-screen justify-center items-center'>
         <div className='bg-white bg-opacity-80 w-2/5 p-16 shadow-xl rounded-xl'>
-            <h1 className='text-5xl text-black font-medium text-center mb-16'>Artfolio</h1>
-            <div className='flex flex-col gap-4 mb-10'>
-                
-                <input type="email" placeholder='E-mail' onChange={handleChangeEmail}
-                className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
+            <h1 className='text-5xl text-black font-medium text-center'>Artfolio</h1>
+            <p className='text-black text-center pt-10 pb-4'>Please fill this form to register new account</p>
+            <div className='flex flex-col gap-4'>
+                    <input type="text" placeholder='Name' 
+                    className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
+                    
+                    <input type="text" placeholder='Username' 
+                    className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
 
-                <input type="password" placeholder='Password' onChange={handleChangePassword}
-                className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
+                    <div className='grid grid-cols-2 gap-4'>
+                        <div className='flex flex-col gap-4'>
+                            <input type="email" placeholder='E-mail' onChange={handleChangeEmail}
+                            className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
+                            
+                            <input type="password" placeholder='Password' onChange={handleChangePassword}
+                            className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
+                            <input type="password" placeholder='Rewrite your password' onChange={handleChangePassword}
+                            className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
+                        </div>
+                        <div className='flex flex-col gap-4'>
+                            <input type="text" placeholder='Phone Number' 
+                            className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
+
+                            <input type="text" placeholder='Bio' 
+                            className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
+
+                            <input type="text" placeholder='Website' 
+                            className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
+                        </div>
+                    </div>
 
                 <button type="submit" onClick={handleRegister}
-                className='py-3 bg-orange text-white hover:bg-peach hover:text-black font-medium rounded-md'>
+                className='my-4 py-3 bg-orange text-white hover:bg-peach hover:text-black font-medium rounded-md'>
                     Register
                 </button>
                 <div className="flex gap-1 justify-center md:text-sm text-xs">
                     <p className='text-black'>Already have an account?</p>
                     <Link to='/login' className="underline text-orange hover:text-peach cursor-pointer">Log in</Link>
                 </div>
-
             </div>
         </div>
     </div>
