c74fe9f74e7735773dcc576c49305339d7248f30
indialifah <indialifah25@gmail.com>
2024-09-24 09:56:34 +0700

    set color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        whitey: '#FDF6E3',
+        black: '#333333',
+        orange: '#D35400',
+        peach: '#F7C8A7',
+        sand: '#FFEEDB',
+        teal: '#008080',
+      }
+    },
   },
   plugins: [],
 }
