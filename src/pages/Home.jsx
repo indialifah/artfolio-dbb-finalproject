44b2877d6a31e9e57962b6b12f7c3a5f78792c30
indialifah <indialifah25@gmail.com>
2024-09-26 20:12:37 +0700

    edit layout Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,19 +9,22 @@ const Home = () => {
         {/* Container untuk timeline */}
         <div className='mt-24 w-[60%] mx-auto'>
           {/* Ini satu Post */}
-          <div className='bg-white shadow-lg rounded-md py-4 px-6 my-4'>
-            <div className='flex flex-col gap-4'>
+          <div className='bg-white shadow-lg rounded-md p-6 my-4'>
+            <div className='flex flex-col gap-6'>
                 {/* user */}
-                <div className='flex gap-4'>
-                  <div className='w-10 h-10 bg-peach rounded-full'></div>
-                  <p className='text-lg leading-9'>username</p>
+                <div className='flex justify-between'>
+                  <div className='flex gap-4 cursor-pointer'>
+                    <div className='w-10 h-10 bg-peach rounded-full'></div>
+                    <p className='text-lg leading-9'>username</p>
+                  </div>
+                  <div>
+                    <p className='leading-9 px-2 text-sm border-[1px] border-solid border-black rounded-lg cursor-pointer'>followed</p>
+                  </div>
                 </div>
                 {/* photo carousel */}
-                <div className='flex gap-1'>
-                  <div className='w-80 h-56 bg-peach rounded-md'></div>
-                  <div className='w-80 h-56 bg-peach rounded-md'></div>
-                  <div className='w-80 h-56 bg-peach rounded-md'></div>
-                  <div className='w-80 h-56 bg-peach rounded-md'></div>
+                <div className='flex gap-6'>
+                  <div className='w-3/5 h-[420px] bg-peach rounded-md'></div>
+                  <p>Description</p>
                 </div>
                 {/* likes comments */}
                 <div className='flex gap-8'>
