e67f60aba7a1c3003d2741080f29553bb486bb6d
indialifah <indialifah25@gmail.com>
2024-09-29 23:48:50 +0700

    add Story component

---
+++ src/components/Story.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Story = () => {
+  return (
+    <div>
+        <div className='flex gap-10 py-6 mx-auto w-[60%]'>
+            <div className='flex flex-col items-center'>
+                <div className='w-16 h-16 bg-peach rounded-full border-solid border-4 border-orange'></div>
+                <p className='text-sm'>username</p>
+            </div>
+            <div className='flex flex-col items-center'>
+                <div className='w-16 h-16 bg-peach rounded-full border-solid border-4 border-orange'></div>
+                <p className='text-sm'>username</p>
+            </div>
+            <div className='flex flex-col items-center'>
+                <div className='w-16 h-16 bg-peach rounded-full border-solid border-4 border-orange'></div>
+                <p className='text-sm'>username</p>
+            </div>
+            <div className='flex flex-col items-center'>
+                <div className='w-16 h-16 bg-peach rounded-full border-solid border-4 border-orange'></div>
+                <p className='text-sm'>username</p>
+            </div>
+            <div className='flex flex-col items-center'>
+                <div className='w-16 h-16 bg-peach rounded-full border-solid border-4 border-orange'></div>
+                <p className='text-sm'>username</p>
+            </div>
+            <div className='flex flex-col items-center'>
+                <div className='w-16 h-16 bg-peach rounded-full border-solid border-4 border-orange'></div>
+                <p className='text-sm'>username</p>
+            </div>
+            <div className='flex flex-col items-center'>
+                <div className='w-16 h-16 bg-peach rounded-full border-solid border-4 border-orange'></div>
+                <p className='text-sm'>username</p>
+            </div>
+            <div className='flex flex-col items-center'>
+                <div className='w-16 h-16 bg-peach rounded-full border-solid border-4 border-orange'></div>
+                <p className='text-sm'>username</p>
+            </div>
+            <div className='flex flex-col items-center'>
+                <div className='w-16 h-16 bg-peach rounded-full border-solid border-4 border-orange'></div>
+                <p className='text-sm'>username</p>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Story
