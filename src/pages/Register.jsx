c0cc163d65833edac8fcfbb16822c405ccbe58fd
indialifah <indialifah25@gmail.com>
2024-09-30 10:32:44 +0700

    fix register function

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
@@ -13,13 +14,31 @@ const Register = () => {
   const [website, setWebsite] = useState('')
   const [profilePicUrl, setProfilePicUrl] = useState('')
 
+  const handleChangeName = (e) => {
+    setName(e.target.value)
+  }
+  const handleChangeUsername = (e) => {
+    setUsername(e.target.value)
+  }
   const handleChangeEmail = (e) => {
     setEmail(e.target.value)
   }
-
   const handleChangePassword = (e) => {
     setPassword(e.target.value)
   }
+  const handleChangePasswordRepeat = (e) => {
+    setPasswordRepeat(e.target.value)
+  }
+  const handleChangePhoneNumber = (e) => {
+    setPhoneNumber(e.target.value)
+  }
+  const handleChangeBio = (e) => {
+    setBio(e.target.value)
+  }
+  const handleChangeWebsite = (e) => {
+    setWebsite(e.target.value)
+  }
+  
 
   const handleRegister = () => {
     const payload = {
@@ -34,7 +53,23 @@ const Register = () => {
       profilePictureUrl: profilePicUrl,
     }
 
-    console.log(payload)
+    const config = {
+      headers: {
+        "Content-Type" : "application/json",
+        "apiKey" : 'c7b411cc-0e7c-4ad1-aa3f-822b00e7734b'
+      }
+    }
+
+    axios
+      .post("https://photo-sharing-api-bootcamp.do.dibimbing.id/api/v1/register", payload, config)
+      .then((res) => {
+        console.log(payload)
+        console.log(res.data)
+        // add success toast redirect to login
+      })
+      .catch((err) => {
+        console.log(err.response)
+      })
   }
 
   return (
@@ -43,36 +78,52 @@ const Register = () => {
             <h1 className='text-5xl text-black font-medium text-center'>Artfolio</h1>
             <p className='text-black text-center pt-10 pb-4'>Please fill this form to register new account</p>
             <div className='flex flex-col gap-4'>
-                    <input type="text" placeholder='Name' 
+                    <input type="text" placeholder='Name' onChange={handleChangeName} required
                     className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
                     
-                    <input type="text" placeholder='Username' 
+                    <input type="text" placeholder='Username' onChange={handleChangeUsername} required
                     className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
 
                     <div className='grid grid-cols-2 gap-4'>
                         <div className='flex flex-col gap-4'>
-                            <input type="email" placeholder='E-mail' onChange={handleChangeEmail}
+                            <input type="email" placeholder='E-mail' onChange={handleChangeEmail} required
                             className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
                             
-                            <input type="password" placeholder='Password' onChange={handleChangePassword}
+                            <input type="password" placeholder='Password' onChange={handleChangePassword} required
                             className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
-                            <input type="password" placeholder='Rewrite your password' onChange={handleChangePassword}
+                            <input type="password" placeholder='Rewrite your password' onChange={handleChangePasswordRepeat} required
                             className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
                         </div>
                         <div className='flex flex-col gap-4'>
-                            <input type="text" placeholder='Phone Number' 
+                            <input type="text" placeholder='Phone Number' onChange={handleChangePhoneNumber}
                             className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
 
-                            <input type="text" placeholder='Bio' 
+                            <input type="text" placeholder='Bio' onChange={handleChangeBio}
                             className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
 
-                            <input type="text" placeholder='Website' 
+                            <input type="text" placeholder='Website' onChange={handleChangeWebsite}
                             className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
                         </div>
                     </div>
 
-                <input type="file" src="" alt="profile pic" placeholder='Profile Picture'
-                className=''/>
+                {/* <input type="file" src="" alt="profile pic" placeholder='Profile Picture'
+                className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/> */}
+
+                <div className="relative">
+                      <label
+                        htmlFor="file-upload"
+                        className="block rounded-md p-2 w-full text-gray-400 bg-white border-2 border-gray-200 cursor-pointer focus:outline-none focus:border-orange text-left"
+                      >
+                        {/* {fileName || 'Profile Picture'} Placeholder text */}
+                        Profile Picture
+                      </label>
+                      <input
+                        id="file-upload"
+                        type="file"
+                        className="absolute top-0 left-0 opacity-0 w-full h-full cursor-pointer"
+                        // onChange={handleFileChange}
+                      />
+                </div>
 
                 <button type="submit" onClick={handleRegister}
                 className='mt-4 py-3 bg-orange text-white hover:bg-peach hover:text-black font-medium rounded-md'>
