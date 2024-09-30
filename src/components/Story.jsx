608c93b36bb5a1b6fe0215bb37d63ab9e3173078
indialifah <indialifah25@gmail.com>
2024-10-01 00:40:59 +0700

    apply overflow and hide scrollbar to Story component

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Story = () => {
   return (
     <div>
-        <div className='flex gap-10 py-6 mx-auto w-[60%]'>
+        <div className='flex gap-10 py-6 mx-auto w-3/5 overflow-x-auto no-scrollbar'>
             <div className='flex flex-col items-center'>
                 <div className='w-16 h-16 bg-peach rounded-full border-solid border-4 border-orange'></div>
                 <p className='text-sm'>username</p>
