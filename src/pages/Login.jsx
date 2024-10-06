3b17b0b4beb7417649b66d91082093048e478f32
indialifah <indialifah25@gmail.com>
2024-10-07 01:30:40 +0700

    handle errors on Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,10 +2,16 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 const Login = () => {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  const [errors, setErrors] = useState({})
 
   const navigate = useNavigate()
 
@@ -18,29 +24,44 @@ const Login = () => {
   }
 
   const handleLogin = () => {
+    const validationErrors = {}
+    if (!email) validationErrors.email = 'Email is required'
+    if (!password) validationErrors.password = "Password is required"
+
+    if (Object.keys(validationErrors).length > 0) {
+      // console.log(validationErrors)
+      setErrors(validationErrors)
+      return
+    }
+
     const payload = {
       email: email,
       password: password,
     }
     
     const config = {
-        headers: {
-          "Content-Type" : "application/json",
-          "apiKey" : 'c7b411cc-0e7c-4ad1-aa3f-822b00e7734b'
+      headers: {
+        "Content-Type" : "application/json",
+        "apiKey" : 'c7b411cc-0e7c-4ad1-aa3f-822b00e7734b'
       }
-  }
+    }
+
     axios
       .post("https://photo-sharing-api-bootcamp.do.dibimbing.id/api/v1/login", payload, config)
       .then((res) => {
         console.log(res.data)
         localStorage.setItem('access_token', res?.data?.token)
-
+        setErrors('')
+        toast.success("Login successful! 🎉")
         setTimeout(() => {
           navigate('/')
-        }, 3000)
+        }, 2000)
       })
       .catch((err) => {
         console.log(err.response)
+        toast.error(`Login failed! Please try again.`)
+        const serverErrors = err?.response?.data?.message || {};  
+        setErrors(serverErrors);  // Set errors ke response error atau tetap objek kosong
       })
   }
 
@@ -52,11 +73,17 @@ const Login = () => {
             <p className='text-black text-center pt-10 pb-4'>Please fill this form to log in to your account</p>
             <div className='flex flex-col gap-4'>
                 
-                <input type="email" placeholder='E-mail' onChange={handleChangeEmail}
+                <input type="email" placeholder='E-mail' onChange={handleChangeEmail} required
                 className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
+                {errors.email && <span className="text-red-500 text-sm">{errors.email}</span>}
 
-                <input type="password" placeholder='Password' onChange={handleChangePassword}
+                <input type="password" placeholder='Password' onChange={handleChangePassword} required
                 className='rounded-md p-2 w-[100%] focus:outline-none focus:border-orange border-2'/>
+                {errors.password && <span className="text-red-500 text-sm">{errors.password}</span>}
+
+                {typeof errors === 'string' && (
+                  <span className="text-red-500 text-md text-center font-medium">{errors}</span>
+                )}
 
                 <button type="submit" onClick={handleLogin}
                 className='my-4 py-3 bg-orange text-white hover:bg-peach hover:text-black font-medium rounded-md'>
@@ -69,6 +96,9 @@ const Login = () => {
 
             </div>
         </div>
+
+        
+        <ToastContainer className='rounded-xl' autoClose={2000}/>
     </div>
     
   )
