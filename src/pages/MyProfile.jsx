4741f19ae05bd545f5505367ea99c8d56387d015
indialifah <indialifah25@gmail.com>
2024-10-08 01:34:29 +0700

    get logged user data from api

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../components/Sidebar'
+import axios from 'axios'
+import EditProfileModal from '../components/EditProfileModal'
 
 const MyProfile = () => {
+
+  const [user, setUser] = useState(null)
+  const [isEditProfileModalOpen, setIsEditProfileModalOpen] = useState(false)
+
+  const openEditProfileModal = () => {
+    setIsEditProfileModalOpen(true)
+  }
+
+  const closeEditProfileModal = () => {
+    setIsEditProfileModalOpen(false)
+  }
+
+  const getLoggedUser = () => {
+    
+    const token = localStorage.getItem('access_token')
+    const config = {
+      headers: {
+        'apiKey': 'c7b411cc-0e7c-4ad1-aa3f-822b00e7734b',
+        'Authorization': `Bearer ${token}`,
+      }
+    }
+
+    axios
+    .get('https://photo-sharing-api-bootcamp.do.dibimbing.id/api/v1/user', config)
+    .then((res) => {
+      console.log(res.data.data)
+      setUser(res?.data?.data)
+    })
+    .catch((err) => {
+      console.log(err.response)
+    })
+  }
+
+  useEffect(() => {
+    getLoggedUser()
+  })
+
   return (
     <div>
         <Sidebar />
@@ -12,11 +51,11 @@ const MyProfile = () => {
                 {/* User Profile */}
                 <div className='flex justify-between'>
                   <div className='flex gap-10'>
-                      <div className='w-36 h-36 bg-peach rounded-full'></div>
+                      <img src={user?.profilePictureUrl} className='w-36 h-36 object-cover bg-peach rounded-full'></img>
                       <div  className='flex flex-col gap-6'>
                         <div>
-                          <p className='text-3xl font-bold text-black'>Name's gallery</p>
-                          <p className='text-xl text-black'>Username</p>
+                          <p className='text-3xl font-bold text-black'>{user?.name}</p>
+                          <p className='text-xl text-black'>{user?.username}</p>
                         </div>
                         <div className='flex gap-4'>
                           <div className='text-center'>
@@ -24,18 +63,18 @@ const MyProfile = () => {
                             <p>Posts</p>
                           </div>
                           <div className='text-center'>
-                            <p className='text-2xl font-light'>20K</p>
+                            <p className='text-2xl font-light'>{user?.totalFollowers}</p>
                             <p>Followers</p>
                           </div>
                           <div className='text-center'>
-                            <p className='text-2xl font-light'>10</p>
-                            <p>Followings</p>
+                            <p className='text-2xl font-light'>{user?.totalFollowing}</p>
+                            <p>Following</p>
                           </div>
                         </div>
                       </div>
                   </div>
                   <div>
-                    <p className='leading-9 px-2 text-sm border-[1px] border-solid border-peach hover:bg-sand rounded-lg cursor-pointer'>Edit Profile</p>
+                    <div onClick={openEditProfileModal} className='leading-9 px-2 text-sm border-[1px] border-solid border-peach hover:bg-sand rounded-lg cursor-pointer'>Edit Profile</div>
                   </div>
                 </div>
 
@@ -46,22 +85,31 @@ const MyProfile = () => {
                   <div>
                     <p className='text-lg font-medium italic'>Bio</p>
                     <p className='text-md'>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      {user?.bio}  
                     </p>
                   </div>
                   {/* Contacts: web, phoneNum */}
                   <div className='flex gap-4'>
-                    <a href="" className='underline font-semibold text-teal'>website hyperlink</a>
+                    <a href='' className='underline font-semibold text-teal'>{user?.email}</a>
                     <p>|</p>
-                    <a href='' className='underline font-semibold text-teal'>phone number</a>
+                    <a href='' className='underline font-semibold text-teal'>{user?.website}</a>
+                    <p>|</p>
+                    <a href='' className='underline font-semibold text-teal'>{user?.phoneNumber}</a>
                   </div>
                 </div>
 
                 <hr className='my-6'/>
 
                 {/* User Gallery */}
+                <div>
+                  <p className='text-lg font-medium italic'>My Posts</p>
+                </div>
             </div>
         </div>
+
+        {isEditProfileModalOpen && (
+          <EditProfileModal closeModal={closeEditProfileModal} />
+        )}
     </div>
   )
 }
